11e285db047401a311f5564853ca5772076cc993
Nielsen Alonso <nielsenwebdev@gmail.com>
2022-10-16 11:28:33 -0400

    done Close #9

--- src/hooks/usePokemons.js
+++ src/hooks/usePokemons.js
@@ -4,40 +4,43 @@ import { fetchData } from '../api';
 import { RandomNumber } from '../helpers/RandomNumber';
 import { ShuffleArray } from '../helpers/ShuffleArray';
 
-const usePokemons = (cantMax) => {
+const usePokemons = () => {
 	const [pokemons, setPokemons] = useState([]);
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState(null);
 
-	useEffect(() => {
-		const getData = async () => {
-			try {
-				const pokes = [];
-				setLoading(true);
+	const getData = async () => {
+		try {
+			const pokes = [];
+			setLoading(true);
 
-				while (pokes.length < cantMax) {
-					const result = await fetchData(RandomNumber(1, 151));
-					if (!pokes.find((arr) => arr.name === result.name))
-						pokes.push(result);
-				}
-
-				const data = [...pokes, ...pokes];
-				const result = ShuffleArray(data);
-
-				setPokemons(result);
-				setError(null);
-				setLoading(false);
-			} catch (error) {
-				setError(error);
-				setLoading(false);
-				setPokemons([]);
+			while (pokes.length < 6) {
+				const result = await fetchData(RandomNumber(1, 151));
+				if (!pokes.find((arr) => arr.name === result.name)) pokes.push(result);
 			}
-		};
 
+			const data = [...pokes, ...pokes];
+			const result = ShuffleArray(data);
+
+			setPokemons(result);
+			setError(null);
+			setLoading(false);
+		} catch (error) {
+			setError(error);
+			setLoading(false);
+			setPokemons([]);
+		}
+	};
+
+	const reload = () => {
 		getData();
-	}, [cantMax]);
+	};
 
-	return { pokemons, loading, error };
+	useEffect(() => {
+		getData();
+	}, []);
+
+	return { pokemons, loading, error, reload };
 };
 
 export { usePokemons };
