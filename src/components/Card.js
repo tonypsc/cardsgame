d88d5c3b7c1e724400c325517f599f58ca7845de
Nielsen Alonso <nielsenwebdev@gmail.com>
2022-10-16 01:56:43 -0400

    change of shape of card and flip funcionaliti ok

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,31 +1,30 @@
 import styles from './Card.module.css';
 import pokeballImg from '../assets/images/card-back.png';
 
-const Card = ({ pokemon, status, onClick, index }) => {
+const Card = ({ pokemon, onClick, index, status }) => {
 	return (
-		<div className={styles.cardContainer}>
-			<div className={styles.card}>
-				{status === 'normal' ? (
-					<div className={styles.cardBack}>
-						<img
-							onClick={(e) => onClick(e.target.name, index)}
-							src={pokeballImg}
-							alt="back-img"
-							name={pokemon.name}
-						/>
-					</div>
-				) : (
-					<div className={styles.cardFront}>
-						<div className={styles.cardFrontContent}>
-							<div className={styles.pokemonPhoto}>
-								<img
-									src={pokemon.sprites.other.dream_world.front_default}
-									alt="pokemon-img"
-								/>
-							</div>
-						</div>
-					</div>
-				)}
+		<div
+			className={
+				status === 'normal' ? styles.card : `${styles.card} ${styles.flipped}`
+			}
+		>
+			<div className={styles.front}>
+				<img
+					src={pokemon.sprites.other.dream_world.front_default}
+					alt="pokemon-img"
+				/>
+			</div>
+			<div
+				className={
+					status === 'normal' ? styles.back : `${styles.back} ${styles.flipped}`
+				}
+			>
+				<img
+					onClick={(e) => onClick(e.target.name, index)}
+					src={pokeballImg}
+					alt="back-img"
+					name={pokemon.name}
+				/>
 			</div>
 		</div>
 	);
